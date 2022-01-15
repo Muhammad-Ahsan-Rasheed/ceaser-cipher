4972dd0a4a4517aff522f43fdcf7cbfcfe61344b
Muhammad Ahsan <69692532+Muhammad-Ahsan-Rasheed@users.noreply.github.com>
2022-01-15 14:55:32 +0500

    Update main.py
    ceaser cipher v2.0.... In this version I've added file handling.

--- main.py
+++ main.py
@@ -6,10 +6,23 @@ def decrypt(c_text, key):
     alphabets = "abcdefghijklmnopqrstuvwxyz"
     return "".join([alphabets[alphabets.index(i) - key] if i != " " else " "  for i in c_text])
 
-plain_text = input("Enter Plain Text : ")
+plain_text = ""
+name = input('Enter File Name to Encrypt: ')
+try:
+    with open(name+'.txt', 'r') as f:
+        plain_text = f.read()
+        print('File read Successfully!')
+        print(f'Data is "{plain_text[:7]} ..."')
+except FileNotFoundError:
+    print('File Not Found!')
+    print('Program Exiting...')
+    exit()
+
 key = int(input("Enter Key: ")) % 26
 
 cipher = encrypt(plain_text, key)
-
-print("Cipher Text :", cipher)
-print("Plain Text :", decrypt(cipher, key))
+with open(name+'.cpr', 'w') as fb:
+    fb.write(cipher)
+    print('File Encrypted Successfully!')
+    print(f'File Saved as {name}.cpr')
+    fb.close()

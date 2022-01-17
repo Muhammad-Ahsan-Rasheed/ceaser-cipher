07a2e9e9583f930560f545b70f15109819dbf763
Muhammad Ahsan <69692532+Muhammad-Ahsan-Rasheed@users.noreply.github.com>
2022-01-17 18:28:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
+from string import ascii_lowercase
+
 def encrypt(text, key):
     alphabets = "abcdefghijklmnopqrstuvwxyz"
-    return "".join([alphabets[(alphabets.index(i) + key) % 26] if i != " " else " " for i in text])
+    return "".join([alphabets[(alphabets.index(i) + key) % 26] if i in ascii_lowercase else i for i in text])
 
 def decrypt(c_text, key):
     alphabets = "abcdefghijklmnopqrstuvwxyz"
-    return "".join([alphabets[alphabets.index(i) - key] if i != " " else " "  for i in c_text])
+    return "".join([alphabets[alphabets.index(i) - key] if i in ascii_lowercase else i  for i in c_text])
 
-plain_text = ""
-name = input('Enter File Name to Encrypt: ')
-try:
-    with open(name+'.txt', 'r') as f:
-        plain_text = f.read()
-        print('File read Successfully!')
-        print(f'Data is "{plain_text[:7]} ..."')
-except FileNotFoundError:
-    print('File Not Found!')
-    print('Program Exiting...')
-    exit()
+def filing(in_name, key):
+    plain_text = ''
 
-key = int(input("Enter Key: ")) % 26
+    try:
+        with open(in_name, 'r') as f:
+            plain_text = f.read()
+            print('File read Successfully!')
+            print(f'Data is "{plain_text[:7]} ..."')
+    except FileNotFoundError:
+        print('File Not Found!')
+        print('Program Exiting...')
+        exit()
+    cipher = encrypt(plain_text, key)
+    
+    out_file_name = name.split('.')[0]+'.cpr'
+    with open(out_file_name, 'w') as fb:
+        fb.write(cipher)
+        print('File Encrypted Successfully!')
+        print(f'File Saved as {out_file_name}')
+        fb.close()
 
-cipher = encrypt(plain_text, key)
-with open(name+'.cpr', 'w') as fb:
-    fb.write(cipher)
-    print('File Encrypted Successfully!')
-    print(f'File Saved as {name}.cpr')
-    fb.close()
+if __name__ == '__main__':
+    name = input('Enter File Name (with extension e.g file.txt) to Encrypt: ')
+    key = int(input("Enter Key: ")) % 26
+
+    filing(name, key)
